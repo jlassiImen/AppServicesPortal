0df9a53a21e994e7f2851e27891a12cee3c74852
safwen <schibeni@jasmineconseil.com>
2019-05-26 23:42:25 +0200

    geolocation work

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,10 +14,44 @@ import 'rxjs/add/operator/catch';
 export class HomeComponent implements OnInit {
   
   temperature="";
+  longitude ="";
+  latitude="";
+  city="";
   constructor(public auth: AuthService, private http: HttpClient) { }
 
   ngOnInit() {
+    this.getUserPosition();
+    
   	this.getCityMeteo();
+  }
+
+  getCityName():void{
+     var geocodeURL='https://maps.googleapis.com/maps/api/geocode/json?latlng=' + this.latitude+','+this.longitude+'&key=AIzaSyADDo_r8NSK8D0JYDl3f_UBRg4iS6ut-cY';
+    return this.http.get(geocodeURL)
+	.subscribe(
+        (data: any) => {
+        	//console.log(JSON.stringify(data));
+            this.city = JSON.stringify(data);
+           
+        },
+        err => console.log(err), // error
+    );;
+
+  }
+
+  getUserPosition():void{
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position)=>{
+        console.log(position);
+          this.longitude = position.coords.longitude;
+          this.latitude = position.coords.latitude;
+          this.getCityName();
+        });
+    } else {
+       console.log("No support for geolocation")
+    }
+  
+
   }
   getCityMeteo(){
     var meteoURL="https://api.openweathermap.org/data/2.5/weather?q=paris,fr&appid=c21a75b667d6f7abb81f118dcf8d4611&units=metric";

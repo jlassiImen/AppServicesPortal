0e21e726b129d0c56cd738e3ef405656f0632d01
safwen <schibeni@jasmineconseil.com>
2019-06-01 00:24:44 +0100

    fix to deploy to heroku

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-
-//Install express server
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/cityMeteo'));
-
-app.get('*', function (req, res) {
- const index = path.join(__dirname, '/dist/cityMeteo/index.html');
- 
- res.sendFile(index);
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const express = require('express');  
+const app = express();  
+app.use(express.static(__dirname + '/dist'));  
+app.all('*', (req, res) => {  
+  res.status(200).sendFile(__dirname + '/dist/index.html');  
+});  
+app.listen(process.env.PORT || 8080); 

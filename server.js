90a70dc5c78fddd8bb06b569c8241ae00271ac06
safwen <schibeni@jasmineconseil.com>
2019-05-31 20:52:24 +0100

    fix to deploy to heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/cityMeteo'));
 
 app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'index.html');
+  const index = path.join(__dirname, '/dist/cityMeteo/index.html');
   res.sendFile(index);
 });
 

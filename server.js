e6fbe6263d1ef6a660b9a773e7970cfcb7747a56
safwen <schibeni@jasmineconseil.com>
2019-05-31 21:26:18 +0100

    fix to deploy to heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/cityMeteo/dist'));
 
 app.get('*', function (req, res) {
-  const index = path.join(__dirname, '/dist/index.html');
+  const index = path.join(__dirname, '/dist/cityMeteo/index.html');
   res.sendFile(index);
 });
 

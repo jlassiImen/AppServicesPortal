b897850b0d1174f0e8c978c68a886472cc6c3897
safwen <schibeni@jasmineconseil.com>
2019-06-01 00:33:51 +0100

    fix to deploy to heroku

--- src/app/services/auth/auth0-variables.ts
+++ src/app/services/auth/auth0-variables.ts
@@ -7,5 +7,5 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: 'RXtKNr033L5b38rUC1eyDjm3MQbquujL',
   domain: 'dev-oih21c7f.eu.auth0.com',
-  callbackURL: 'http://localhost:3000/callback'
+  callbackURL: 'https://city-meteo.herokuapp.com/callback'
 };

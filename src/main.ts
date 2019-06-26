efe84edb9ee8756743de4eaf37482d2807714a8b
safwen <schibeni@jasmineconseil.com>
2019-06-26 11:39:17 +0200

    resoudre l'erreur

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { LazymoduleModule } from './app/lazymodule/lazymodule.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
@@ -9,3 +10,4 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(LazymoduleModule);

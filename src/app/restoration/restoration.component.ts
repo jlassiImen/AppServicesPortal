532e64d66d45117e824d329f41c8fe44feeeaf27
safwen <schibeni@jasmineconseil.com>
2019-07-24 21:59:22 +0200

    add resto list filter

--- src/app/restoration/restoration.component.ts
+++ src/app/restoration/restoration.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, Input, OnInit, ViewChild, } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, FormControl, ValidatorFn } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from './../services/auth/auth.service';
 import { MeteoService } from './../services/meteoServices/meteo.service';
@@ -23,31 +23,127 @@ export class RestorationComponent implements OnInit {
   @Input() q: string;
   @ViewChild('autocomplete') qElementRef: ElementRef;
 
-  private places: any;
+  private places: any; //autocomplete adress
 
 
-  constructor(public auth: AuthService, public router: Router, public fb: FormBuilder, public restoration: RestorationService, public meteo: MeteoService) {}
+
+  //form
+  form: FormGroup;
+  typeRestaurant=[
+    {
+      name: "American",
+      id: 1
+    },
+    {
+      name: "Italian",
+      id: 2
+    },
+    {
+      name: "Frensh",
+      id: 3
+    },
+    {
+      name: "Traditional",
+      id: 4
+    },
+    {
+      name: "Pizzaria",
+      id: 5
+    },
+    {
+      name: "coffee",
+      id: 6
+    }
+  ];
+  sortBy = [
+    {
+      name: "Price",
+      value: "Price",
+      selected: true
+    },
+    {
+      name: "Note",
+      value: "Note"
+    }
+  ];
+  distance = [
+    {
+      name: "Less of 500 m",
+      id: 10
+    },
+    {
+      name: "By car (8 km)",
+      id: 11
+    },
+    {
+      name: "By bike (4 km)",
+      id:12
+    },
+    {
+      name: "walk (2 km)",
+      id: 13
+    }
+  ];
+  features = [
+    {
+      name: "Open now",
+      id: 20
+    },
+    {
+      name: "Accept reservation",
+      id: 21
+    },
+    {
+      name: "delivery ",
+      id: 22
+    }
+  ];
+  neighborhoods = [
+    {
+      name: "3000 m",
+      id: 30
+    },
+    {
+      name: "6000 m",
+      id: 31
+    },
+    {
+      name: "15000 ",
+      id: 32
+    },
+    {
+      name: "30000 ",
+      id: 33
+    },
+    {
+      name: "40000 ",
+      id: 34
+    }
+  ];
   
-  restaurantForm: FormGroup;
-  restaurants = ['Open now', 'Pizzaria', 'coffee',  'American',  'Frensh', 'Italian', 'Halal', 'Indian', 'Japanese']
+  //onsubmit result
+  restaurantList : Observable<any[]> 
+  p: Number = 1; //pagination
 
-  restaurantList : Observable<any[]>
-  p: Number = 1;
-
-  restoForm: FormGroup;
   successMessage = '';
   errorMessage = '';
-  validation_messages = {
-    'typeRestaurant': [
-      { type: 'required' }
-    ],
-    'adresse': [
-      { type: 'required' }
-    ]
+
+
+  constructor(public auth: AuthService, public router: Router, public fb: FormBuilder,public restoration: RestorationService, public meteo: MeteoService) {
+
+    this.form = this.fb.group({
+      typeRestaurant: new FormArray([]),
+      sortBy: new FormArray([]),
+      distance: new FormArray([]),
+      features: new FormArray([]),
+      neighborhoods: new FormArray([])
+    });
+
+    this.addCheckboxes();
   }
 
+
   ngOnInit() {
-   
     // address autocomplete
     this.places = places({
       appId:  environment.autoCompleteAppId,
@@ -55,62 +151,73 @@ export class RestorationComponent implements OnInit {
       container: this.qElementRef.nativeElement,
       style: false,
       debug: true
-  });
-  if (this.q) {
+     });
+    if (this.q) {
       this.places.setVal(this.q);
-  }
-  this.places.on('change', function resultSelected(e) {
+    }
+    this.places.on('change', function resultSelected(e) {
       console.log("eeeeeeeeeee "+JSON.stringify(e));
-  });
-
-    var req = {
+    });
+  this.meteo.detectLocation(position => this.initMap(position));
+  var req = {
   "term":"restaurant",
   "location" : "16 rue des acacias 92360 meudon"
   }
-    this.restaurantList=this.restoration.getYelpRestaurants(req);
-    this.createForms();
-    this.meteo.detectLocation(position => this.initMap(position));
+ this.restaurantList=this.restoration.getYelpRestaurants(req); 
   }
 
- 
-  createForms() {
-    this.restoForm = this.fb.group({
-      typeRestaurant: [''],
-      address: new FormControl('', Validators.compose([
-        Validators.required
-      ]))
-    })
+  private addCheckboxes() {
+    this.typeRestaurant.map((o, i) => {
+      const control = new FormControl(i === 0); // if first item set to true, else false
+      (this.form.controls.typeRestaurant as FormArray).push(control);
+    });
+    this.sortBy.map((o, i) => {
+      const control = new FormControl(i === 0); // if first item set to true, else false
+      (this.form.controls.sortBy as FormArray).push(control);
+    });
+    this.distance.map((o, i) => {
+      const control = new FormControl(i === 0); // if first item set to true, else false
+      (this.form.controls.distance as FormArray).push(control);
+    });
+    this.features.map((o, i) => {
+      const control = new FormControl(i === 0); // if first item set to true, else false
+      (this.form.controls.features as FormArray).push(control);
+    });
+    this.neighborhoods.map((o, i) => {
+      const control = new FormControl(i === 0); // if first item set to true, else false
+      (this.form.controls.neighborhoods as FormArray).push(control);
+    });
   }
 
   onSubmitResto(value) {
-  console.log("rrrrrrrrrrrrrr       "+JSON.stringify(value));
+    const selectedTypeRestaurantIds = this.form.value.typeRestauranttypeRestaurant
+      .map((v, i) => v ? this.typeRestaurant[i].id : null)
+      .filter(v => v !== null);
+  
+  
+  console.log("rrrrrrrrrrrrrr       "+selectedTypeRestaurantIds);
   var request ={
   "term":"restaurant",
   "location" : "Promenade du 7e Art, 77200 Torcy"
 }
 this.restaurantList=this.restoration.getYelpRestaurants(request);
-  }
+}
 
-
-  initMap(position) {
+initMap(position) {
  
-    console.log(position.longitude + ' , ' + position.latitude);
-    const map = L.map('map').setView([position.latitude, position.longitude], 12);
- 
-    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',
-    { id: 'mapbox.streets', attribution: '', maxZoom: 20, accessToken: this.accessToken , tileSize: 512, zoomOffset: -1} as any
+ console.log(position.longitude + ' , ' + position.latitude);
+ const map = L.map('map').setView([position.latitude, position.longitude], 12);
+ L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',
+ { id: 'mapbox.streets', attribution: '', maxZoom: 20, accessToken: this.accessToken , tileSize: 512, zoomOffset: -1} as any
   ).addTo(map);
-    const myIcon = L.icon({
-      iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
-    });
-
-
-    L.marker([position.latitude, position.longitude], {icon: myIcon}).bindPopup('Your position').addTo(map).openPopup();
-
-    for (let key in this.restaurantList) {
-      let restaurant = this.restaurantList[key];
-      // Use `key` and `value`
-      L.marker([restaurant.coordinates.latitude, restaurant.coordinates.longitude], {icon: myIcon})
+ const myIcon = L.icon({
+ iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
+ });
+ L.marker([position.latitude, position.longitude], {icon: myIcon}).bindPopup('Your position').addTo(map).openPopup();
+ for (let key in this.restaurantList) {
+    let restaurant = this.restaurantList[key];
+    // Use `key` and `value`
+    L.marker([restaurant.coordinates.latitude, restaurant.coordinates.longitude], {icon: myIcon})
   }
 
 
--- src/app/restoration/restoration.module.ts
+++ src/app/restoration/restoration.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
-import { FormBuilder, FormGroup, Validators, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, FormControl , Validators, ReactiveFormsModule } from '@angular/forms';
 import {NgxPaginationModule} from 'ngx-pagination';
 import { RestorationRoutingModule } from './restoration-routing.module';
 import { RestorationComponent } from './restoration.component';
